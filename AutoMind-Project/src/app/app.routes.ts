9de230ab8b79456cd061571b9a05d772af99c008
Andrej <146711514+AndrejNedic@users.noreply.github.com>
2025-10-08 12:37:12 +0200

    now it works thst the home component is the default starting component

--- AutoMind-Project/src/app/app.routes.ts
+++ AutoMind-Project/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full' }
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: Home }
 ];
