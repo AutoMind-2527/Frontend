d43a20c4b7c8850e0d932cb10dbd1ee1eac3bb0f
Andrej <146711514+AndrejNedic@users.noreply.github.com>
2025-10-16 14:15:32 +0200

    made a component dashboard and fixed the routing system

--- AutoMind-Project/src/app/app.routes.ts
+++ AutoMind-Project/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Dashboard } from './dashboard/dashboard';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: Home }
+    { path: 'home', component: Home },
+    { path: 'dashboard', component: Dashboard}
 ];
