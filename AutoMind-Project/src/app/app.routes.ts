bbedc244b226d52ed7c7a340025a038b5accf188
Andrej <146711514+AndrejNedic@users.noreply.github.com>
2025-10-16 14:20:21 +0200

    added a learn-more component

--- AutoMind-Project/src/app/app.routes.ts
+++ AutoMind-Project/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Dashboard } from './dashboard/dashboard';
+import { LearnMoreComponent } from './learn-more.component/learn-more.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: Home },
-    { path: 'dashboard', component: Dashboard}
+    { path: 'dashboard', component: Dashboard},
+    { path: 'learn-more', component: LearnMoreComponent }
 ];
