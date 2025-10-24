671f0bc7b3e6c6e014d1ae4b3d1ade0ca4f1a798
mBindreiter-HTL <m.bindreiter@htblaleonding.onmicrosoft.com>
2025-10-24 14:11:54 +0200

    added the map to live map

--- AutoMind-Project/src/app/live-map/live-map.ts
+++ AutoMind-Project/src/app/live-map/live-map.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-live-map',
-  standalone: true,
   templateUrl: './live-map.html',
   styleUrls: ['./live-map.css']
 })
-export class LiveMapComponent {
-  
+export class LiveMapComponent implements AfterViewInit {
+  private map!: L.Map;
+
+  constructor(private router: Router) {}
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [48.3069, 14.2858], // Beispiel: Linz
+      zoom: 13
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+
+    const marker = L.marker([48.3069, 14.2858]).addTo(this.map);
+    marker.bindPopup('<b>AutoMind Vehicle</b><br>Aktuelle Position').openPopup();
+  }
+
+  goHome(): void {
+    this.router.navigate(['/home']);
+  }
 }
