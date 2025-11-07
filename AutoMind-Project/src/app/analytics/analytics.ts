0cd2df40d776a57873a09d7791a41ea501675c37
mBindreiter-HTL <m.bindreiter@htblaleonding.onmicrosoft.com>
2025-11-07 14:23:28 +0100

    started implementation of the analytics (not far yet css problems)

--- AutoMind-Project/src/app/analytics/analytics.ts
+++ AutoMind-Project/src/app/analytics/analytics.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-analytics',
   standalone: true,
+  imports: [RouterModule],
   templateUrl: './analytics.html',
   styleUrls: ['./analytics.css']
 })
-export class AnalyticsComponent {
-  
-}
+export class AnalyticsComponent {}
