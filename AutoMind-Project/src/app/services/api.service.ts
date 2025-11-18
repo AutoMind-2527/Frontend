33c9d9e2df1e14ef90fce2af9abc3201ff1d0b80
mBindreiter-HTL <m.bindreiter@htblaleonding.onmicrosoft.com>
2025-11-18 14:55:37 +0100

    api service ts so the backend and the forntend is can be connected (no data transmission yet)

---
+++ AutoMind-Project/src/app/services/api.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class ApiService {
+  private apiUrl = 'http://localhost:5000'; // Backend Port
+  
+  constructor(private http: HttpClient) {}
+  
+  getData() {
+    return this.http.get(`${this.apiUrl}/api/controller`);
+  }
+}
